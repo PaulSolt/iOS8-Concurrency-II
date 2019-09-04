253ff97356d554e4df0c85207e9aa0239e7e3790
Paul Solt <PaulSolt@gmail.com>
2019-09-04 12:41:29 -0400

    Added lock demo

--- iOS8-Concurrency-II.playground/Pages/Basic Locks.xcplaygroundpage/Contents.swift
+++ iOS8-Concurrency-II.playground/Pages/Basic Locks.xcplaygroundpage/Contents.swift
@@ -1,6 +1,21 @@
 import UIKit
 
-var str = "Hello, playground"
+//: Create a shared resource and modify it from multiple concurrent threads
 
+var x = 42 // Shared resource
+var xLock = NSLock()
 
+//for i in 0..<100 {
+DispatchQueue.concurrentPerform(iterations: 100) { (index) in
+//    xLock.lock()
+    var localCopy = x   // Read
+    localCopy += 1      // Modify
+    x = localCopy       // Write
+    print("x: \(x)")
+//    xLock.unlock()
+}
+// 6 cores = 12 threads (2 threads per core)
+// 4 cores = 8
+// 2 cores = 4
+print("End: expecting x to be: 142: \(x)")
 //: [Next](@next)
