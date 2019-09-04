6b59a73a4612ee9dad645c73e9f2bf060f84de9e
Paul Solt <PaulSolt@gmail.com>
2019-09-04 13:31:41 -0400

    Added first demo with lock contention

--- iOS8-Concurrency-II.playground/Pages/Lock Contention.xcplaygroundpage/Contents.swift
+++ iOS8-Concurrency-II.playground/Pages/Lock Contention.xcplaygroundpage/Contents.swift
@@ -2,12 +2,42 @@
 
 import Foundation
 
+// GCD
+// DispatchQueue = grocery store checkout line
+// serial = 1 lane
+// concurrent = 2+ lanes (12 lanes = 6 core CPU)
+
+// DispatchGroup = chaporone - count the heads of all the kids on the field
+
+var sharedResource = 0
+let lock = NSLock()
+let group = DispatchGroup() // .init() // Option-click to see QuickHelp
+
+let numberOfIterations = 20_000 // 20,000  20000
+
+var startTime = Date()
+
+// Do work
+print("Start")
+for _ in 0 ..< numberOfIterations {
+    group.enter()   // entering the field trip destination
+    
+    // Lock vs. DispatchQueue
+    lock.lock()
+    
+    sharedResource += 1
+    
+    lock.unlock()
+    
+    group.leave()   // get on the bus to go home
+}
+
+print("Waiting")
+group.wait()        // wait until all the kids are on the bus
 
 
-
-
-
-
-
+var endTime = Date()
+var elapsedTime = endTime.timeIntervalSinceReferenceDate - startTime.timeIntervalSinceReferenceDate
+print("Time elapsed to add \(numberOfIterations): \(elapsedTime) seconds")
 
 //: [Next](@next)
