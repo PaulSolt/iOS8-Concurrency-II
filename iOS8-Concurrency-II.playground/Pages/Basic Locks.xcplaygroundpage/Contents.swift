376aa4098bf9ce909df998a4bf7e6e50004d8d42
Paul Solt <PaulSolt@gmail.com>
2019-09-04 12:44:51 -0400

    Added lock

--- iOS8-Concurrency-II.playground/Pages/Basic Locks.xcplaygroundpage/Contents.swift
+++ iOS8-Concurrency-II.playground/Pages/Basic Locks.xcplaygroundpage/Contents.swift
@@ -6,13 +6,15 @@ var x = 42 // Shared resource
 var xLock = NSLock()
 
 //for i in 0..<100 {
-DispatchQueue.concurrentPerform(iterations: 100) { (index) in
-//    xLock.lock()
+ DispatchQueue.concurrentPerform(iterations: 100) { (index) in
+    xLock.lock()
+    
     var localCopy = x   // Read
     localCopy += 1      // Modify
     x = localCopy       // Write
-    print("x: \(x)")
-//    xLock.unlock()
+//    print("x: \(x)")
+    
+    xLock.unlock()
 }
 // 6 cores = 12 threads (2 threads per core)
 // 4 cores = 8
